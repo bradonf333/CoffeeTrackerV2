e344c2d267bf54531e2b13b28da8798a05d3e5f0
bfredrickson <bfredrickson@healthequity.com>
2019-06-18 14:31:29 -0600

    Added coffee details.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { CoffeeDetailsComponent } from './coffee-details/coffee-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'coffee-details', component: CoffeeDetailsComponent }
+  // { path: 'hero/:id', component: HeroDetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoffeeDetailsComponent } from './coffee-details/coffee-details.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CoffeeDetailsComponent],
   imports: [BrowserModule, AppRoutingModule, MDBBootstrapModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent]
