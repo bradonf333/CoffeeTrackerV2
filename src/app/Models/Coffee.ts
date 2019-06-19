3372f5f77c0fa2f25a80f124734f4b4e546d6858
bfredrickson <bfredrickson@healthequity.com>
2019-06-19 09:53:59 -0600

    Stub a coffee list and adding new coffee to list.

---
+++ src/app/Models/Coffee.ts
@@ -0,0 +1,19 @@
+export class Coffee {
+  id?: string;
+  name: string;
+  roaster: string;
+  // roastDate: string;
+  // regions: string[];
+  rating: number;
+  // flavors: string[];
+  // TODO: This will be for future use. Once I start tracking daily espresso shots, I will use this.
+  // date: string;
+  // notes?: string;
+
+  constructor(name: string, roaster: string, rating: number) {
+    this.id = '123';
+    this.name = name;
+    this.roaster = roaster;
+    this.rating = rating;
+  }
+}
--- src/app/Services/coffee.service.ts
+++ src/app/Services/coffee.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Coffee } from '../Models/Coffee';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoffeeService {
+  public coffees: Coffee[] = [
+    { id: '1', name: 'El Vapor', roaster: 'Jack Mormon', rating: 7 },
+    { id: '2', name: 'La Bicicletta', roaster: 'Doma Coffee Roasters', rating: 7.5 },
+    { id: '3', name: 'Little Italy', roaster: 'Bird Rock', rating: 9 }
+  ];
+  constructor() {}
 
-  constructor() { }
+  addNewCoffee(name: string, roaster: string, rating: number) {
+    this.coffees.push(new Coffee(name, roaster, rating));
+  }
 }
